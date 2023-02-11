bc7c423c631e03a14ff502fd1378a9737606f4db
Vinicius Souza <ipooo346@gmail.com>
2023-02-11 16:38:16 -0300

    fixed notation

--- code/population/population.rb
+++ code/population/population.rb
@@ -2,14 +2,13 @@ startsize = ARGV[0].to_f
 endsize = ARGV[1].to_f
 
 def timeElapsed(startsize, endsize)
-    i = 0
     years = 0
     population = startsize
 
     while population + population/12 < endsize
         population += population/12
         years += 1
-        puts "Years: #{years}, Population: #{population}"
+        puts "Year: #{years}, Population: #{population}"
     end
 
     puts "Years: #{years + population/endsize}"
@@ -19,7 +18,7 @@ if (startsize.is_a? Float) && (endsize.is_a? Float) && (startsize > 2)
     timeElapsed(startsize, endsize)
 else
     STDERR.puts <<-eof
-    INVALID ARGUMENT
-    startsize must be most than 2
+    INVALID ARGUMENT:
+        Start size must be greater than 2;
     eof
 end
